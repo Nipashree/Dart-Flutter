5ec413f894c4ac67e4e60bbd882208c6217fe870
Nipashree Bhattacharya <146835960+Nipashree@users.noreply.github.com>
2025-11-04 15:16:51 +0530

    basic Dart program with variable declarations

---
+++ Basics_1.dart
@@ -0,0 +1,20 @@
+void main() {
+  String name="mario";
+   
+  
+//  
+//   final age=25;
+  int age=26;
+  print("my name is $name and my age is $age");
+  
+  bool isOpen=true;
+  print(isOpen);
+  
+  double avg=9;
+  avg=9.5;
+  print(avg);
+  
+  int? point;
+  
+  print(point);
+  }
